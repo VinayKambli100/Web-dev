1c33f225787ec7fa6934c6143fbd1c48b1e0eabf
VinayKambli100 <105406619+VinayKambli100@users.noreply.github.com>
2022-08-01 13:15:23 +0530

    Add files via upload

---
+++ Office learning/javascript/change background color/index.js
@@ -0,0 +1,10 @@
+// function bluecolor()
+// {
+//     document.getElementById("body_ele").className = "blue";
+// }
+
+var bluecolor = () => document.getElementById("body_ele").className = "blue";
+var redcolor = () => document.getElementById("body_ele").className = "red";
+var yellowcolor = () => document.getElementById("body_ele").className = "yellow";
+var greencolor = () => document.getElementById("body_ele").className = "green";
+  
