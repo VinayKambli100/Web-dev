7ae973579dd3654456d847684c04bc1e7cf16e6e
VinayKambli100 <105406619+VinayKambli100@users.noreply.github.com>
2022-08-04 11:08:45 +0530

    Add files via upload

---
+++ Office learning/vue2/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <home/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Home,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Office learning/vue2/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1> Hello this text from Home.vue  {{"hello there".length}}</h1>
+    <h1>{{email}}</h1>
+    <h1>{{mobile}}</h1>
+    <h1>{{getName()}}</h1>
+    <h1>{{addnum(50,30)}}</h1>
+</template>
+
+<script>
+export default {
+    name:"my_home",  //why is this line use for ???
+    data(){
+        return{
+            email:"example@gmail.com",
+            mobile:9999,
+            getName()
+            {
+                return "hello there retrun form fucntion"
+            },
+            addnum(a,b)
+            {
+                let c = a+b
+                return c;
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1{
+    color: blue;
+}
+</style>
